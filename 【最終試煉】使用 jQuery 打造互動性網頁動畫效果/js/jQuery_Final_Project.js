60e05199adc73c3f826c3a046fd554fb849faf75
yoshiyyc <95995009+yoshiyyc@users.noreply.github.com>
2022-01-12 22:06:20 +0800

    Updated 【最終試煉】使用 jQuery 打造互動性網頁動畫效果

--- 【最終試煉】使用 jQuery 打造互動性網頁動畫效果/js/jQuery_Final_Project.js
+++ 【最終試煉】使用 jQuery 打造互動性網頁動畫效果/js/jQuery_Final_Project.js
@@ -1,20 +1,13 @@
 $(document).ready(function () {
-    $(".dropdown").mouseenter(function (e) { 
+    $(".dropdown").mouseenter(function (e) {
         e.preventDefault();
         $(this).addClass("nav__link--active");
         $(this).siblings(".nav-submenu").slideDown();
     });
 
-    $(".nav-menu__item").mouseleave(function () { 
+    $(".nav-menu__item").mouseleave(function () {
         $(".dropdown").removeClass("nav__link--active");
         $(".nav-submenu").slideUp(200);
-    });
-
-    $(".top-link").click(function (e) { 
-        e.preventDefault();
-        $("html, body").animate({
-            scrollTop: 0
-        }, 700);
     });
 
     const swiper = new Swiper('.swiper', {
@@ -36,7 +29,24 @@ $(document).ready(function () {
     lightbox.option({
         'resizeDuration': 200,
         'wrapAround': true,
-        'fadeDuration':300,
+        'fadeDuration': 300,
         'imageFadeDuration': 300
-      })
+    })
+
+    $(".top-link").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: 0
+        }, 700);
+    });
+
+    function showBtnCondition() {
+        if ($(this).scrollTop() > 850) {
+            $(".top-link").fadeIn();
+        } else {
+            $(".top-link").fadeOut();
+        }
+    }
+    $(window).scroll(showBtnCondition);
+
 });
