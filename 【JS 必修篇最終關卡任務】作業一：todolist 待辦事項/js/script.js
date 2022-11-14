41bbb549e4685d3db7902650dc3d0c7b6fbff50b
Yung Ching Yang (Yoshi) <95995009+yoshiyyc@users.noreply.github.com>
2022-11-14 11:12:42 +0800

    Update js based on TA's suggestions
    - updateTaskArr 可以直接根據要渲染的資料狀態 filter 就好。例如先設一個 status 預設是 all，點 tab 時會切換，呼叫 updateTaskArr 就根據 status 篩資料，此時 decideArray 也可以移除了
    - createTask 可以直接改到 renderData 做，不需再存 taskAll
    - 沒有用到的變數可以移除，例如：JS 第 5 - 7 行

--- 【JS 必修篇最終關卡任務】作業一：todolist 待辦事項/js/script.js
+++ 【JS 必修篇最終關卡任務】作業一：todolist 待辦事項/js/script.js
@@ -2,209 +2,158 @@ const btnAdd = document.querySelector(".btn--add");
 const inputBox = document.querySelector(".input-box");
 const tab = document.querySelector(".tab");
 const tabCollection = document.querySelectorAll(".tab__item");
-const tabAll = document.querySelector(".tab--all");
-const tabPending = document.querySelector(".tab--pending");
-const tabCompleted = document.querySelector(".tab--completed");
 const list = document.querySelector(".list");
 const reminderPending = document.querySelector(".reminder--pending");
 const deleteAll = document.querySelector(".delete-all");
 
-var todoArr = [];
-
-var taskAll = [];
-var taskPending = [];
-var taskCompleted = [];
+let todoArr = [];
+let tabStatus = "all";
 
 //Click - Enter todo items (Add button)
 btnAdd.addEventListener("click", (e) => {
-  addToDo();
+    addToDo();
 });
 
 //Click(Keypress) - Enter todo items (Enter Key)
 inputBox.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    addToDo();
-  }
+    if (e.key === "Enter") {
+        addToDo();
+    }
 });
 
 //Function - Create todo objects (set status)
 function addToDo() {
-  if (inputBox.value.trim() === "") {
-    alert("請輸入代辦事項");
-  }
-  else {
-    const todo = {
-      text: inputBox.value,
-      checked: false,
-      id: Date.now()
-    };
-
-    todoArr.push(todo);
-    createTask(todo);
-    updateTaskArr();
-    inputBox.value = "";
-  }
-}
-
-//Function - Create task content using todo status
-function createTask(todo) {
-  const task = document.createElement("li");
-
-  task.setAttribute("class", `task`);
-  task.setAttribute("data-key", todo.id);
-  task.innerHTML = `
-    <label class="checkbox" for="${todo.id}">
-      <input id=${todo.id} class="task__input" type="checkbox">
-      <span>${todo.text}</span>
-    </label>
-    <a class="delete" href="#"></a>
-  `;
-
-  taskAll.unshift(task);
-};
-
-//Function - Update tasks shown
-function updateTaskArr() {
-  const allID = [];
-  const completedID = [];
-  const pendingID = [];
-
-  taskCompleted = [];
-  taskPending = [];
-
-  var tempArr = [];
-
-  //Mark and categorize the ids of todos
-  todoArr.forEach(i => {
-    allID.push(i.id);
-
-    if (i.checked === true) {
-      completedID.push(i.id);
+    if (inputBox.value.trim() === "") {
+        alert("請輸入代辦事項");
     }
     else {
-      pendingID.push(i.id);
+        const todo = {
+            text: inputBox.value,
+            checked: false,
+            id: Date.now()
+        };
+
+        todoArr.unshift(todo);
+        updateTodoArr();
+        inputBox.value = "";
     }
-  });
-
-  //Update tasks using categorized todo-ids
-  taskAll.forEach(task => {
-    allID.forEach(id => {
-      if (Number(task.dataset.key) === id) {
-        tempArr.push(task);
-      }
-    })
-
-    completedID.forEach(id => {
-      if (Number(task.dataset.key) === id) {
-        taskCompleted.push(task);
-      }
-    });
-
-    pendingID.forEach(id => {
-      if (Number(task.dataset.key) === id) {
-        taskPending.push(task);
-      }
-    });
-
-  });
-
-  taskAll = tempArr;
-
-  decideArray();
-
-  //Update number of pending tasks
-  reminderPending.innerHTML = `${taskPending.length} 個待完成項目`;
 }
 
-//Function - Decide which task array to show 
-function decideArray() {
-  tabCollection.forEach(i => {
-    if (i.classList.contains("tab__item--active")) {
-      if (i.classList.contains("tab--all")) {
-        renderData(taskAll);
-      }
-      else if (i.classList.contains("tab--pending")) {
-        renderData(taskPending);
-      }
-      else {
-        renderData(taskCompleted);
-      }
+//Function - Update todoss shown
+function updateTodoArr() {
+    const completedTodoArr = [];
+    const pendingTodoArr = [];
+
+    //Create arrays for different todo status
+    todoArr.forEach(i => {
+        if (i.checked === true) {
+            completedTodoArr.push(i);
+        }
+        else {
+            pendingTodoArr.push(i);
+        }
+    });
+
+    //Update tasks using categorized todos
+    if (tabStatus === "pending") {
+        renderData(pendingTodoArr);
     }
-  });
+    else if (tabStatus === "completed") {
+        renderData(completedTodoArr);
+    }
+    else {
+        renderData(todoArr);
+    }
+
+    //Update number of pending todos
+    reminderPending.innerHTML = `${pendingTodoArr.length} 個待完成項目`;
 }
 
 //Function - Render 
-function renderData(taskArr) {
-  list.innerHTML = "";
-  taskArr.forEach(i => {
-    list.append(i);
-  });
+function renderData(todoArr) {    
+    list.innerHTML = todoArr.map(todo => {        
+        return `
+        <li id=${todo.id} class="task">
+            <label class="checkbox" for="${todo.id}content">
+                <input id="${todo.id}content" class="task__input" type="checkbox" ${todo.checked ? "checked" : ""}>
+                <span>${todo.text}</span>
+            </label>
+            <a class="delete" href="#"></a>
+        </li>
+    `;
+    }).join("");
 };
 
 
-//Click - Check off and delete tasks
+//Click - Check off and delete todos
 list.addEventListener("click", (e) => {
-  //Click - Check
-  if (e.target.parentElement.classList.contains("checkbox")) {
-    const key = e.target.parentElement.parentElement.dataset.key;
-    toggleDone(key);
-  }
-
-  //Click - Delete
-  if (e.target.classList.contains("delete")) {
-    const key = e.target.parentElement.dataset.key;
-    deleteToDo(key);
-  }
-
-  updateTaskArr();
-});
-
-//Function - Check off task
-function toggleDone(key) {
-  todoArr.forEach(i => {
-    if (i.id === Number(key)) {
-      i.checked = !i.checked;
+    //Click - Check
+    if (e.target.parentElement.classList.contains("checkbox")) {
+        const key = e.target.parentElement.parentElement.getAttribute("id");
+        toggleDone(key);
     }
-  });
 
-  updateTaskArr();
-}
+    //Click - Delete
+    if (e.target.classList.contains("delete")) {
+        const key = e.target.parentElement.getAttribute("id");
+        deleteToDo(key);
+    }
 
-//Function - Delete task
-function deleteToDo(key) {
-  const index = todoArr.findIndex(i => {
-    return i.id === Number(key)
-  });
-  todoArr = todoArr.filter(i => {
-    return i.id !== Number(key)
-  });
-  taskAll = taskAll.filter(i => {
-    return i.dataset.key !== key
-  });
-}
-
-//Click - Delete all completed tasks
-deleteAll.addEventListener("click", (e) => {
-  deleteAllComp();
-  updateTaskArr();
+    updateTodoArr();
 });
 
-//Function - Delete all completed tasks {
+//Function - Check off todo
+function toggleDone(key) {
+    todoArr.forEach(i => {
+        if (i.id === Number(key)) {
+            i.checked = !i.checked;
+        }
+    });
+}
+
+//Function - Delete todos
+function deleteToDo(key) {
+    todoArr = todoArr.filter(i => {
+        return i.id !== Number(key);
+    });
+}
+
+//Click - Delete all completed todos
+deleteAll.addEventListener("click", (e) => {
+    deleteAllComp();
+    updateTodoArr();
+});
+
+//Function - Delete all completed todos
 function deleteAllComp() {
-  todoArr = todoArr.filter(i => {
-    return i.checked === false;
-  });
+    todoArr = todoArr.filter(i => {
+        return i.checked === false;
+    });
 }
 
 
 //Click - change tab
 tab.addEventListener("click", (e) => {
-  tabCollection.forEach(i => {
-    i.classList.remove("tab__item--active");
-  });
+    tabCollection.forEach(i => {
+        i.classList.remove("tab__item--active");
+    });
 
-  e.target.focus();
-  e.target.classList.add("tab__item--active");
+    e.target.focus();
+    e.target.classList.add("tab__item--active");
 
-  decideArray();
+    tabCollection.forEach(i => {
+        if (i.classList.contains("tab__item--active")) {
+            if (i.classList.contains("tab--all")) {
+                tabStatus = "all";
+            }
+            else if (i.classList.contains("tab--pending")) {
+                tabStatus = "pending";
+            }
+            else {
+                tabStatus = "completed";
+            }
+        }
+    });
+
+    updateTodoArr();
 });
