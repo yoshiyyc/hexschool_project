38e192d0bc817fbec15910b2d62bc022613f0848
Yung Ching Yang (Yoshi) <95995009+yoshiyyc@users.noreply.github.com>
2022-11-15 23:12:28 +0800

    Upload renamed html & css files for GitHub Pages

---
+++ 【最終試煉】使用 jQuery 打造互動性網頁動畫效果/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $(".dropdown").mouseenter(function (e) {
+        e.preventDefault();
+        $(this).addClass("nav__link--active");
+        $(this).siblings(".nav-submenu").slideDown();
+    });
+
+    $(".nav-menu__item").mouseleave(function () {
+        $(".dropdown").removeClass("nav__link--active");
+        $(".nav-submenu").slideUp(200);
+    });
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        loop: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        speed: 1200,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        'fadeDuration': 300,
+        'imageFadeDuration': 300
+    })
+
+    $(".top-link").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: 0
+        }, 700);
+    });
+
+    function showBtnCondition() {
+        if ($(this).scrollTop() > 850) {
+            $(".top-link").fadeIn();
+        } else {
+            $(".top-link").fadeOut();
+        }
+    }
+    $(window).scroll(showBtnCondition);
+
+});
