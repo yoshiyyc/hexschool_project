32c0e5deb2bfe062b24b92a3f6e05f9aaaa8681b
Yung Ching Yang (Yoshi) <95995009+yoshiyyc@users.noreply.github.com>
2022-11-16 13:12:31 +0800

    Updated the folder and files
    Improvements based on TA's feedback + renamed files

---
+++ 後台管理 - 功能性為導向的介面規劃/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    const toggleMenuBtn = document.querySelector("#toggle-btn");
+    const body = document.querySelector("body");
+    toggleMenuBtn.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        body.classList.toggle("sidebar-toggled");
+    })
+
+});
+
+
